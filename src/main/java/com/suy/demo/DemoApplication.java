d5c29247da32ce5b318d5f78cbdc35962d3541fc
Running_Water_20170826 <yangguang914@163.com>
2019-06-13 15:57:58 +0800

    init repo

---
+++ src/main/java/com/suy/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.suy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
